13eb1dc5f9bfda5587b776a7e5007f2935bf579c
KiraYagamov <isaru@sasuke.ru>
2023-01-07 13:30:58 +0300

    мы пиво пьем, мы любим пиво! большой мы в пиве видим смысл...
    поправил баги, улучшил оптимизацию, добавил зелье регенерации, добавил переход на 9 уровни, добавил указание денег при продаже, добавил экран загрузки при загрузке новой игры или продолжения старой

--- Assets/Scripts/Player/menu/RespawnManager.cs
+++ Assets/Scripts/Player/menu/RespawnManager.cs
@@ -9,45 +9,43 @@ public class RespawnManager : MonoBehaviour
     [SerializeField] private MobController[] mobs;
 
     private void Start() {
-        for(int i = 0; i < mobs.Length; i++){
-            if(PlayerPrefs.GetString("MobTimeDie"  + mobs[i].id + mobs[i].num) == ""){
-                mobs[i].hp = mobs[i].maxHp;
-                mobs[i].gameObject.SetActive(true);
-                mobs[i].bars.SetActive(true);
+        //StartCoroutine("CheckRespawns");
+        CheckItems();
+        CheckMobs();
+    }
+    private void CheckItems(){
+        DateTime nowTime = DateTime.Now;
+        for(int i = 0; i < items.Length; i++){
+            if(PlayerPrefs.GetString("CollectTimeItem" + items[i].type + items[i].id + items[i].num) != ""){
+                if(nowTime > DateTime.Parse(PlayerPrefs.GetString("CollectTimeItem" + items[i].type + items[i].id + items[i].num)).AddMinutes(items[i].delay)){
+                    items[i].gameObject.SetActive(true);
+                }
+                else{
+                    items[i].gameObject.SetActive(false);
+                }
             }
             else{
-                mobs[i].gameObject.SetActive(false);
-                mobs[i].bars.SetActive(false);
+                items[i].gameObject.SetActive(true);
             }
         }
-        StartCoroutine("CheckRespawns");
+    }
+    private void CheckMobs(){
+        DateTime nowTime = DateTime.Now;
+        for(int i = 0; i < mobs.Length; i++){
+            if(PlayerPrefs.GetString("MobTimeDie" + mobs[i].id + mobs[i].num) != ""){
+                if(nowTime > DateTime.Parse(PlayerPrefs.GetString("MobTimeDie"  + mobs[i].id + mobs[i].num)).AddMinutes(mobs[i].delay) && mobs[i].gameObject.activeSelf == false){
+                    mobs[i].hp = mobs[i].maxHp;
+                    mobs[i].gameObject.SetActive(true);
+                    mobs[i].bars.SetActive(true);
+                }
+            }
+        }
     }
     private IEnumerator CheckRespawns() {
         while(true){
-            DateTime nowTime = DateTime.Now;
-            for(int i = 0; i < items.Length; i++){
-                if(PlayerPrefs.GetString("CollectTimeItem" + items[i].type + items[i].id + items[i].num) != ""){
-                    if(nowTime > DateTime.Parse(PlayerPrefs.GetString("CollectTimeItem" + items[i].type + items[i].id + items[i].num)).AddMinutes(items[i].delay)){
-                        items[i].gameObject.SetActive(true);
-                    }
-                    else{
-                        items[i].gameObject.SetActive(false);
-                    }
-                }
-                else{
-                    items[i].gameObject.SetActive(true);
-                }
-            }
-            for(int i = 0; i < mobs.Length; i++){
-                if(PlayerPrefs.GetString("MobTimeDie"  + mobs[i].id + mobs[i].num) != ""){
-                    if(nowTime > DateTime.Parse(PlayerPrefs.GetString("MobTimeDie"  + mobs[i].id + mobs[i].num)).AddMinutes(mobs[i].delay) && mobs[i].gameObject.activeSelf == false){
-                        mobs[i].hp = mobs[i].maxHp;
-                        mobs[i].gameObject.SetActive(true);
-                        mobs[i].bars.SetActive(true);
-                    }
-                }
-            }
-            yield return new WaitForSeconds(5f);
+            CheckItems();
+            CheckMobs();
+            yield return new WaitForSeconds(10f);
         }
     }
 }
