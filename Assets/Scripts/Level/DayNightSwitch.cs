b40b510c37f24c427c81e87399a82d4a736f0670
KiraYagamov <isaru@sasuke.ru>
2023-01-16 20:13:48 +0300

    ой
    улучшил систему работы времени, что-то поделал с качеством графики, добавил консоль, изменил партиклы сбора монет

--- Assets/Scripts/Level/DayNightSwitch.cs
+++ Assets/Scripts/Level/DayNightSwitch.cs
@@ -1,34 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class DayNightSwitch : MonoBehaviour
 {
-    private Animator anim;
-
+    private Light2D light;
+    private float intensity;
+    private float floatTime;
+    private bool toDay;
     private void Start() {
-        anim = GetComponent<Animator>();
-        if(PlayerPrefs.GetInt("Day") == 1){
-            DayCycle();
-        } else {
-            ToDay();
+        toDay = IntToBool(PlayerPrefs.GetInt("ToDay"));
+        light = GetComponent<Light2D>();
+        StartCoroutine(TimeController());
+    }
+    private void Update() {
+        light.intensity = intensity;
+    }
+    private int BoolToInt(bool variable){
+        if(variable != false){
+            return 1;
+        }
+        else{
+            return 0;
         }
     }
-
-    public void ToDay(){
-        anim.SetBool("SunUp", true);
+    private bool IntToBool(int variable){
+        if(variable != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    public void ToNight(){
-        anim.SetBool("SunUp", false);
+    private IEnumerator TimeController(){
+        while(true){
+            if(PlayerPrefs.GetInt("Time") >= 1000){
+                toDay = false;
+            }
+            else if(PlayerPrefs.GetInt("Time") <= 10){
+                toDay = true;
+            }
+            if(toDay){
+                PlayerPrefs.SetInt("Time", PlayerPrefs.GetInt("Time") + 1);
+            }
+            else if(!toDay){
+                PlayerPrefs.SetInt("Time", PlayerPrefs.GetInt("Time") - 1);
+            }
+            floatTime = PlayerPrefs.GetInt("Time");
+            intensity = floatTime/1000;
+            PlayerPrefs.SetInt("ToDay", BoolToInt(toDay));
+            yield return new WaitForSeconds(0.1f);
+        }
     }
-    public void DayCycle(){
-        anim.SetBool("DayCycle", true);
-    }
-    public void SetNight(){
-        PlayerPrefs.SetInt("Day", 0);
-    }
-    public void SetDay(){
-        PlayerPrefs.SetInt("Day", 1);
-    }
-
 }
---
+++ Assets/Scripts/Player/Console.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Console : MonoBehaviour
+{
+    [SerializeField] private GameObject consoleMenu;
+    [SerializeField] private InputField inputField;
+    [SerializeField] private Text text;
+    private string[] wordsInCommand;
+    [SerializeField] private PlayerStats stats;
+    [SerializeField] private TeleportScript tp;
+    public void OpenCloseConsole(){
+        consoleMenu.SetActive(!consoleMenu.activeSelf);
+    }
+    public void EnterCommand(){
+        wordsInCommand = inputField.text.Split(" ");
+        if(wordsInCommand[0] == "give"){
+            if(wordsInCommand[1] == "Default" || wordsInCommand[1] == "Biologic" || wordsInCommand[1] == "Mob" || wordsInCommand[1] == "Mineral"){
+                if(wordsInCommand.Length == 3){
+                    PlayerPrefs.SetInt("Item" + wordsInCommand[1] + wordsInCommand[2], PlayerPrefs.GetInt("Item" + wordsInCommand[1] + wordsInCommand[2]) + 1);
+                }
+                else{
+                    PlayerPrefs.SetInt("Item" + wordsInCommand[1] + wordsInCommand[2], PlayerPrefs.GetInt("Item" + wordsInCommand[1] + wordsInCommand[2]) + int.Parse(wordsInCommand[3]));
+                }
+                text.text = text.text + "\nItem gived!";
+            }
+            else if(wordsInCommand[1] == "Weapon" || wordsInCommand[1] == "Gusenici" || wordsInCommand[1] == "Turbine" || wordsInCommand[1] == "Laser" || wordsInCommand[1] == "Armor"){
+                PlayerPrefs.SetInt(wordsInCommand[1] + wordsInCommand[2], 1);
+                text.text = text.text + "\nModule gived!";
+            }
+            else{
+                text.text = text.text + "\nUnknown variable!";
+            }
+        }
+        else if(wordsInCommand[0] == "heal"){
+            stats.hp = stats.maxHp;
+            text.text = text.text + "\nHealled!";
+        }
+        else if(wordsInCommand[0] == "tp"){
+            tp.Teleport(int.Parse(wordsInCommand[1]));
+            text.text = text.text + "\nTeleported!";
+        }
+        else if(wordsInCommand[0] == "setlevel"){
+            PlayerPrefs.SetInt("PlayerLevel", int.Parse(wordsInCommand[1]));
+            stats.LoadStats();
+            text.text = text.text + "\nLevel setted!";
+        }
+        else if(wordsInCommand[0] == "time"){
+            PlayerPrefs.SetInt("Time", int.Parse(wordsInCommand[1]));
+            text.text = text.text + "\nTime setted!";
+        }
+
+
+
+
+        else{
+            text.text = text.text + "\nUnknown command!";
+        }
+    }
+}
