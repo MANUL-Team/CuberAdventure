40d484622ba5b7f3749b9204c78716860b7cd293
KiraYagamov <isaru@sasuke.ru>
2023-03-21 21:02:51 +0300

    немного доработал(планшет не смотрите :D)

--- Assets/Scripts/Player/Modules/Display/ModulesDisplay.cs
+++ Assets/Scripts/Player/Modules/Display/ModulesDisplay.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ModulesDisplay : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer tracks, laser1, laser2, turbine1, turbine2;
+    [SerializeField] private Transform skins;
     [SerializeField] private ModulesController controller;
     private void FixedUpdate() {
         tracks.sprite = controller._tracks.sprite;
@@ -12,5 +13,13 @@ public class ModulesDisplay : MonoBehaviour
         laser2.sprite = controller._laser.sprite;
         turbine1.sprite = controller._turbine.sprite;
         turbine2.sprite = controller._turbine.sprite;
+        if(PlayerPrefs.GetInt("PlayerRotation") == 1){
+            tracks.flipX = false;
+            skins.rotation = new Quaternion(0, 0, 0, skins.rotation.w);
+        }
+        else if(PlayerPrefs.GetInt("PlayerRotation") == -1){
+            tracks.flipX = true;
+            skins.rotation = new Quaternion(0, 180, 0, skins.rotation.w);
+        }
     }
 }
